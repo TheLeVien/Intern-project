15176c36b81355ccc4c4edefbcd39b994052a89d
Shunival <162007677+Shunival@users.noreply.github.com>
2026-08-14 17:46:47 +0700

    Change app to use environment variable for port

--- app.py
+++ app.py
@@ -60,5 +60,9 @@ def api_compare_routes():
         return jsonify({"error": str(exc)}), 400
     return jsonify(result)
 
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=5000)
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
